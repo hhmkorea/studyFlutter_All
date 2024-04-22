6cc70c79e009e5adf43aa8fafb24c702a7536e45
hhmkorea <yuyee81@gmail.com>
2024-04-22 21:55:10 +0900

    flutter_login : 재사용할 CustomTextFormField 추가

---
+++ flutter_login/lib/components/custom_text_form_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("email"),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: "Enter Email",
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- flutter_login/lib/pages/login_page.dart
+++ flutter_login/lib/pages/login_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/components/Logo.dart';
+import 'package:flutter_login/components/custom_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        // 왼쪽 정렬
-        children: [
-          Logo("Login"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          // 왼쪽 정렬
+          children: [
+            Logo("Login"),
+            CustomTextFormField(),
+          ],
+        ),
       ),
     );
   }
