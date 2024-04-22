4f7f8199e672e606093f3a1878639f1acecf5c6d
hhmkorea <yuyee81@gmail.com>
2024-04-22 11:30:45 +0900

    flutter_store init

---
+++ flutter_store/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea( // 가리지 않는 안전한 지역으로 body 위치함.
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
