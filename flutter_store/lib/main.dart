98abb8f60a31cbb6e8890636c47f7b470ac6ce40
hhmkorea <yuyee81@gmail.com>
2024-04-22 15:25:19 +0900

    flutter_store : 사진보이게 설정

--- flutter_store/lib/main.dart
+++ flutter_store/lib/main.dart
@@ -12,24 +12,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea( // 가리지 않는 안전한 지역으로 body 위치함.
+        body: SafeArea(
+          // 가리지 않는 안전한 지역으로 body 위치함.
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
+              menu(),
+              Expanded(
+                  child: Image.asset("assets/bag.jpeg", fit: BoxFit.cover)),
+              SizedBox(height: 1),
+              Expanded(
+                  child: Image.asset("assets/cloth.jpeg", fit: BoxFit.cover)),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Padding menu() {
+    return Padding(
+      padding: EdgeInsets.all(25.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+          Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+          Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+          Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
       ),
     );
   }
