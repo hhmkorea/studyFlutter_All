dc0663ed76b80089d60e090148fe89386c91c795
hhmkorea <yuyee81@gmail.com>
2024-04-25 21:39:55 +0900

    state_notify_study : main 에러 수정

--- state_notify_study/lib/main.dart
+++ state_notify_study/lib/main.dart
@@ -3,7 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state_notify_study/todo_view_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
