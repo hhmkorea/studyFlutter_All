6942a0b9a945dc13991c26a336628c481ee14f86
hhmkorea <hhmkorea@nate.com>
2025-03-04 21:21:09 +0900

    fixed

--- flutter_airbnb/android/app/src/main/kotlin/com/cos/flutter_airbnb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cos.flutter_airbnb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_profile/android/app/src/main/kotlin/com/cos/flutter_profile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cos.flutter_profile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
